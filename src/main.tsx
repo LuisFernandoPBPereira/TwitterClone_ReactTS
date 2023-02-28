26b91e5c826ac89dd014529ce87aa57575f11326
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-02-28 00:16:14 -0300

    Building the structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from "phosphor-react"
+import "./global.css"
+import logoTwitter from "./assets/logo-twitter.svg"
+import { Tweet } from './Components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img className='logo' src={logoTwitter} alt="Logo" />
+        <nav className='mainNavigation'>
+          <a className='active' href=""> <House weight='fill'/> Home</a>
+          <a href=""> <Hash />           Explore</a>
+          <a href=""> <Bell/>            Notifications</a>
+          <a href=""> <Envelope/>        Messages</a>
+          <a href=""> <BookmarkSimple/>  Bookmarks</a>
+          <a href=""> <FileText/>        Lists</a>
+          <a href=""> <User/>            Profile</a>
+          <a href=""> <DotsThreeCircle/> More</a>
+        </nav>
+        <button className='newTweet' type='button'>Tweet</button>
+      </aside>
+      <div className="content">
+        <main className='timeLine'>
+          <div className="timeLineHeader">
+            Home
+            <Sparkle/>
+          </div>
+          <form action="" className="newTweetForm">
+            <label htmlFor="tweet">
+              <img src="https://github.com/LuisFernandoPBPereira.png" alt="Ícone de Perfil" />
+              <textarea id='tweet' placeholder="What's happening?"></textarea>
+            </label>
+            <button type='submit'>Tweet</button>
+          </form>
+          <div className="separator"></div>
+          <Tweet/>
+          <Tweet/>
+          <Tweet/>
+          <Tweet/>
+        </main>
+      </div>
+    </div>
   </React.StrictMode>,
 )
